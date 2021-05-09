94ebdc388c0d0c23985fbc642d7f7d0fcba3437a
axelle.lanca <axelle.lanca@eemi.com>
2021-05-09 19:59:11 +0200

    new game functions and a test file

--- deck.go
+++ deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // Create a new type of 'deck'
 // wich is a slice of strings
@@ -33,3 +40,50 @@ func (d deck) print() {
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
+
+// To convert a deck to a string
+func (d deck) toString() string {
+	//We convert the deck in a slice of string
+	// To join the slices all together in one string, we use another package called strings
+	return strings.Join([]string(d), ",")
+}
+
+// To save a deck in a local file
+// 0666 permission : anyone can read and modify a file
+func (d deck) saveToFile(filename string ) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+
+func newDeckFromFile(filename string) deck {
+	// ReadFile function can return two values
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - Log the error and return a call to newDeck()
+		// Option #2 - Log the error and entirely quit the program:
+		// we're going to use another package called "os"
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	//We convert the bytes slice into a string, and then we split it to have an array
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
+}
+
+
+func (d deck) shuffle() {
+
+	//To generate a random number 
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) -1)
+		// Swap the elements inside the Slice
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
+
+ 
--- main.go
+++ main.go
@@ -4,12 +4,13 @@ package main
 // La fonction main doit être créer à chaque fois qu'on utilise le package main, la fonctionne sera appelée à chaque fois qu'on execute le programme.
 func main() {
 
+	
+	
+
 	cards := newDeck()
+	cards.shuffle()
+	cards.print()
 
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
 }
 
 
